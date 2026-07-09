daa053783a720b589798febf2907b13d4221b95e
kittipong171299 <kittipong171299@gmail.com>
2026-07-09 09:30:44 +0700

    Add files via upload

---
+++ dec_to_bin.py
@@ -0,0 +1,13 @@
+dec = 1234 #เลขฐาน10
+conv_to = bin(dec) #แปลงเลขฐาน10 to 2
+dec = (conv_to)
+print(dec)
+dec2 = 1234 #เลขฐาน10
+conv_to2 = oct(dec2) #แปลงเลขฐาน10 to 8
+dec2 = (conv_to2)
+print(dec2)
+dec3 = 1234 #เลขฐาน10
+conv_to3 = hex(dec3) #แปลงเลขฐาน10 to 16
+dec3 = (conv_to3)
+print(dec3)
+print('กฤติพงศ์ จันทรโคตร')
